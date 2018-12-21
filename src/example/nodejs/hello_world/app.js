58f6a108ba0e52ce24b2990afe27f5ce158fd59a
y-murakami <y-murakami@oz-vision.co.jp>
2018-12-21 12:44:41 +0900

    test

--- src/example/nodejs/hello_world/app.js
+++ src/example/nodejs/hello_world/app.js
@@ -43,7 +43,7 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'hello world',
+                message: 'hello world 2',
                 location: ret.data.trim()
             })
         }
